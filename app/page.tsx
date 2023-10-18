bf07cc958ec990c6351306350e92be1b46bba690
365-Code <sohailahmedbhati786@gmail.com>
2023-10-18 22:57:44 +0530

    Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import "@/app/scss/index.scss"
 import Navbar from './sections/Navbar'
 import SocialIcons from './components/SocialIcons'
 import Email from './components/Email'
+import Hero from './sections/Hero'
 
 const Home = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Hero/>
       <SocialIcons/>
       <Email/>
     </div>
---
+++ app/sections/Hero.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+import React from 'react'
+import Button from '../components/Button'
+import { Raleway } from 'next/font/google'
+
+const raleway = Raleway({subsets:['latin']})
+
+const Hero = () => {
+  return (
+    <main>
+
+    <div className='hero'>
+        <h1 className="hero-title">
+            Hi my name is
+        </h1>
+        <h2 className={`hero-title-large ${raleway.className}`}>
+            Sohail Ahmed
+        </h2>
+        <h2 className={`hero-title-large hero-title-sub ${raleway.className}`}>
+            I craft things for web.
+        </h2>
+        <p className="hero-text">
+            I&apos;m a student pursuing BTech in Computer Engineering from <Link href={"https://www.jmi.ac.in/"} className='link'>Jamia Millia Islamia</Link>. I am in my learning phase, and making project in the process. I am honing my skills in web development and new things.
+        </p>
+        <div className="hero-button">
+            <Button 
+            text='Check out my latest'
+            link='b-furnitures.vercel.app'/>
+        </div>
+
+    </div>
+    </main>
+  )
+}
+
+export default Hero
